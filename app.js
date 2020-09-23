7f13498c96e4b192dee7e3db8db31d781e255b87
toleen98 <toleenballool98@gmail.com>
2020-09-23 18:31:14 +0300

    add search functionality

---
+++ app.js
@@ -0,0 +1,19 @@
+//onclick search icon => text input appears to search for specific section
+
+ const search = function () {
+    document.getElementById('search').style.visibility = "visible"
+    let input = document.getElementById("search").value;
+
+    //if input has value and value === one of the section it will send user to it otherwise alert will apears
+    if(!input) {
+        return
+    }
+    else if(input && (input === 'team' || input === 'work' || input ==='contact')) {
+        
+         window.location.href ='#'+ input
+    }
+    else {
+        alert('No such section')
+    }
+
+}
